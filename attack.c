a44f925bce8d64e76e03456023f9bd5d9bfd0d06
Luiz Kormann <luizfelipekormann2002@hotmail.com>
2024-06-27 16:52:17 +0200

    square attack function and game moves macro

---
+++ attack.c
@@ -0,0 +1,67 @@
+#include "def.h"
+
+//the square index where x piece would be attacking the square, 120 based 
+const int KnDir[8] = {-8, -19, -21, -12, 8, 19, 21, 12};
+const int RkDir[4] = {-1, -10, 1, 10};
+const int BiDir[4] = {-1, -11, 11, 9};
+const int KiDir[8] = {-1, -10, 1, 10, -9, -11, 11, 9};
+
+int SqAttacked(const int sq, const int side, const S_BOARD *pos){
+
+    int pce, i, t_sq, dir;
+    //pawns
+    if(side == WHITE){
+        if(pos->pieces[sq-11] == wP || pos->pieces[sq-9] ==wP){
+            return TRUE;
+        }
+    }else{
+        if(pos->pieces[sq+11] == bP || pos->pieces[sq+9] ==bP){
+            return TRUE;
+        }
+    }
+    //knights
+    for(i = 0; i < 8; ++i){
+        pce = pos->pieces[sq + KnDir[i]];
+        if(IsKn(pce) && PieceCol[pce] == side){
+            return TRUE;
+        }
+    }
+    //rooks and queens
+    for(i = 0; i < 4; ++i){
+        dir = RkDir[i];
+        pce = pos->pieces[t_sq];
+        while(pce != OFFBOARD){
+            if(pce != EMPTY){
+                if(IsRQ(pce) && PieceCol[pce] == side){
+                    return TRUE;
+                }break;
+            }
+            t_sq += dir;
+            pce = pos->pieces[t_sq];
+        }
+    }
+    //bishops and queens
+    for(i = 0; i < 4; ++i){
+        dir = BiDir[i];
+        pce = pos->pieces[t_sq];
+        while(pce != OFFBOARD){
+            if(pce != EMPTY){
+                if(IsBQ(pce) && PieceCol[pce] == side){
+                    return TRUE;
+                }break;
+            }
+            t_sq += dir;
+            pce = pos->pieces[t_sq];
+        }
+    }
+    //king
+    for(i = 0; i < 8; ++i){
+        pce = pos->pieces[sq + KiDir[i]];
+        if(IsKi(pce) && PieceCol[pce] == side){
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+    
+}
--- data.c
+++ data.c
@@ -5,8 +5,15 @@ char SideChar[] = "wb-";
 char RankChar[] = "12345678";
 char FileChar[] = "abcdefgh";
 
+//arrays to check piece type
 int PieceBig[13] = {FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE}; 
 int PieceMaj[13] = {FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE};
 int PieceMin[13] = {FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE};
 int PieceVal[13] = {0, 100, 325, 325, 550, 1000, 50000, 100, 325, 325, 550, 1000, 50000};
 int PieceCol[13] = {BOTH, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK};
+
+//arrays to check what piece it is attacking the square
+int PieceKnight[13] = {FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE};
+int PieceKing[13] = {FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE};
+int PieceRookQueen[13] = {FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE};
+int PieceBishopQueen[13]= {FALSE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE};
